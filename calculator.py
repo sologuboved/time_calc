8e70fbf8ec5aed97193ab3d6b90d1004956eddf8
sologuboved <sologuboved@gmail.com>
2017-12-11 16:16:19 +0300

    first commit

---
+++ calculator.py
@@ -0,0 +1,27 @@
+import datetime
+
+# TODO input: date, num of days; output: date after num of days elapsed + day of week
+# TODO input: date, num of days; output: date  num of days ago + day of week
+# TODO input: date1, date2; output: num of days between date1 and date2
+# TODO input: time1, time2, time3, ...; output: sum
+
+
+def after():
+    pass
+
+
+def before():
+    pass
+
+
+def between():
+    pass
+
+
+def add():
+    pass
+
+
+if __name__ == '__main__':
+    pass
+    print(datetime)
