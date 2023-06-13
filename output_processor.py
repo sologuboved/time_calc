219ae8f128ebb5f2311e2042ffcd93c0a83566f5
sologuboved <sologuboved@gmail.com>
2023-06-13 18:12:02 +0300

    WIP

--- output_processor.py
+++ output_processor.py
@@ -6,6 +6,10 @@ def output_date(date):
     return f"{date:%d.%m.%Y}"
 
 
+def output_datetimelet(datetimelet):
+    return f"{datetimelet:%d.%m.%Y %H:%M:%S}"
+
+
 def output_days(delta):
     days = delta.days
     if days == 1:
